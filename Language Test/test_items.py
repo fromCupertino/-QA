dd183474ad8e22aa90ef1432b9267f8d44210482
Zakhar Pirozhenko <zpirozhenko@innoseti.ru>
2020-12-23 12:06:50 +0200

    Изменил название функции

--- Language Test/test_items.py
+++ Language Test/test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 
-def test(browser):
+def test_add_to_basket(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     button = WebDriverWait(browser, 15).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[class='btn btn-lg btn-primary btn-add-to-basket']")))
     assert button is not None
